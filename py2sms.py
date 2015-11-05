fd1b2447f4115bae4dc7bb29a6bcc6f3ce6ca75b
scensorECHO <xxartofwarxx@gmail.com>
2015-11-05 03:11:34 -0500

    Updated JSON formatting, service unavailable for testing

--- py2sms.py
+++ py2sms.py
@@ -7,35 +7,31 @@
 #                                                     
 
 
+import json
+import pprint
 import requests
-
 # 'pip install requests' to install this library
 
 
 with open('credentials','r') as c:
-	try:
 		cred = c.readlines()
-	except:
-		print("File could not be read")
-		exit()
-
 
 def sms(pnumber,msg):
-	addr = "https://app.eztexting.com/sending/messages?format=format"
+	addr = "https://app.eztexting.com/sending/messages?format=json"
 	number = str(pnumber)
 	message= str(msg)
-	r = requests.post(addr,data={
-		'User' : cred[0], 
-		'Password' : cred[1]
-		'PhoneNumbers' : [number]
-		'Message' : message})
-	# if r.status_code != 204:
-	# 	if r.json()["success"]:
-	# 		print("Message: \"" + msg + "\" sent to phone number: " + str(pnumber))
-	# 	else:
-	# 		print("Message failed: "+str(r.json()["message"]))
-	# else:
-	# 	print("Status code 204: No content returned")
+	content = json.dumps([{
+		'User': cred[0].strip(), 
+		'Password': cred[1].strip(),
+		'PhoneNumbers': [number],
+		'Message' : message}],
+		separators=(',',':'))
+	pprint.pprint(content)
+	r = requests.post(addr,data=content)
+	if r.status_code != 204:
+		pprint.pprint(r.json())
+	else:
+		print("Status code 204: No content returned")
 
 # To send a text message call the function with
 # sms(phonenumber,'sending this text message')
